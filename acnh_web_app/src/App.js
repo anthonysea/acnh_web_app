ebc02e6b8d21186f0465f3f8298ffcbd03d5f545
anthonysea <anthonyqchao@gmail.com>
2020-04-15 18:21:24 -0700

    Converted App class-component to functional-component with React hooks; implemented fuzzy searching using fuse.js, use-debounce, and React hookds

--- acnh_web_app/src/App.js
+++ acnh_web_app/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import './App.css';
-import { debounce } from 'lodash'
-const fuse = require("fuse.js")
+import { useDebouncedCallback } from 'use-debounce'
+import Fuse from "fuse.js"
 
 const PATH_BASE = 'http://localhost:5000'
 const FISH = '/fish'
@@ -10,81 +10,99 @@ const BUGS = '/bugs'
 const FOSSILS = '/fossils'
 const VILLAGERS = '/villagers'
 
-
-
-class App extends Component{
-
-  constructor(props) {
-    super(props)
-    
-    this.state = {
-      fish: {},
-      bugs: {},
-      fossils: {},
-      villagers: {},
-      searchTerm: "",
+function App() {
+  const [fish, setFish] = useState([])
+  const [bugs, setBugs] = useState([])
+  const [fossils, setFossils] = useState([])
+  const [villagers, setVillagers] = useState([])
+  const searchList = fish.concat(bugs, fossils, villagers)
+  const fuseOpts = {
+    isCaseSensitive: false,
+    shouldShort: true,
+    minMatchCharLength: 3,
+    includeScore: true,
+    threshold: 0.6,
+    keys: [
+      "name"
+    ]
+  }
+  const fuse = new Fuse(searchList, fuseOpts)
+  // Side effect to load in data from back end on component mounting
+  useEffect(() => {
+    const fetchData = async () => {
+      axios.all([
+        axios.get(`${PATH_BASE}${FISH}`),
+        axios.get(`${PATH_BASE}${BUGS}`),
+        axios.get(`${PATH_BASE}${FOSSILS}`),
+        axios.get(`${PATH_BASE}${VILLAGERS}`),
+      ]).then(axios.spread((fishRes, bugsRes, fossilsRes, villagersRes) => {
+        setFish(fishRes.data)
+        setBugs(bugsRes.data)
+        setFossils(fossilsRes.data)
+        setVillagers(villagersRes.data)
+      }))
     }
+    fetchData()
+    console.log("json data changed")
+  }, [])
 
-    this.fetchData = this.fetchData.bind(this)
-    this.fuzzySearch = this.fuzzySearch.bind(this)
+  const [searchResults, setSearchResults] = useState([])
+  const [query, setQuery] = useState("")
+  // Side effect on query to update searchResults array
+  useEffect(() => {
+    console.log(query)
+    setSearchResults(fuse.search(query).slice(0,10))
+    console.log(searchResults)
+  }, [query])
+
+  // Callback to set the query value. This is debounced in the Search component
+  const handleChange = (value) => {
+    console.log("handleChange called, calling setQuery")
+    setQuery(value)
   }
 
-  componentDidMount() {
-    this.fetchData()
-  }
-
-  fetchData() {
-    axios.all([
-      axios.get(`${PATH_BASE}${FISH}`),
-      axios.get(`${PATH_BASE}${BUGS}`),
-      axios.get(`${PATH_BASE}${FOSSILS}`),
-      axios.get(`${PATH_BASE}${VILLAGERS}`),
-    ]).then(axios.spread((fishRes, bugsRes, fossilsRes, villagersRes) => {
-      this.setState({
-        fish: fishRes.data,
-        bugs: bugsRes.data,
-        fossils: fossilsRes.data,
-        villagers: villagersRes.data
-      })
-    }))
-  }
-
-  debounceEventHandler(...args) {
-    const debounced = debounce(...args)
-    return function(e) {
-      e.persist()
-      return debounced(e)
-    }
-  }
-
-  fuzzySearch(event) { 
-    console.log(`fuzzy search called, event.target.value: ${event.target.value}`)
-    this.setState({ searchTerm: event.target.value })
-    
-  }
-
-  render() {
-    return (
+  return (
+    <div>
       <Search
-        onChange={this.debounceEventHandler(this.fuzzySearch, 500)}
+        value={query}
+        onChange={handleChange}
       >
-      Search
+        Search
       </Search>
-    );
-  }
+      <ul>
+        {searchResults && searchResults.map((value, index) => {
+          return <li key={index}>{value.item.name}</li>
+        })}
+      </ul>
+    </div>
+    
+  )
 }
 
-const Search = ({value, onChange, onSubmit, children}) =>
-  <form onSubmit={onSubmit}>
+const Search = ({query, onChange, onSubmit, children}) => {
+
+  const [debouncedCallback] = useDebouncedCallback(onChange, 300)
+
+  return (
+    <form onSubmit={onSubmit}>
     {children}
-    <input type="text"
-      onChange={onChange}
+    {/* <select name="category" id="select-category">
+      {categories.map((value, index) => {
+        return <option key={index} value={value}>{value.charAt(0).toUpperCase() + value.substr(1)}</option>
+      })}
+    </select> */}
+    <input 
+      type="text"
+      value={query}
+      onChange={(e) => debouncedCallback(e.target.value)}
     />
     <button type="submit">
       {children}
     </button>
   </form>
-
+  )
+}
+  
 export default App;
 
 
