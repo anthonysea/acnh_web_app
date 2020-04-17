7fd9b43b248c67735d356eb183c2d1a97eaec8a0
anthonysea <anthonyqchao@gmail.com>
2020-04-17 14:18:22 -0700

    Using react-bootstrap for styling

--- acnh_web_app/src/App.js
+++ acnh_web_app/src/App.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 import './App.css';
-import { useDebouncedCallback } from 'use-debounce'
-import Fuse from "fuse.js"
+import { useDebouncedCallback } from 'use-debounce';
+import Fuse from "fuse.js";
+
+// Bootstrap and React-Bootstrap imports
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup'
+import Button from 'react-bootstrap/Button';
 
 const PATH_BASE = 'http://localhost:5000'
 const FISH = '/fish'
@@ -62,7 +70,7 @@ function App() {
   }
 
   return (
-    <div>
+    <Container className="p-3">
       <Search
         value={query}
         onChange={handleChange}
@@ -74,7 +82,7 @@ function App() {
           return <li key={index}>{value.item.name}</li>
         })}
       </ul>
-    </div>
+    </Container>
     
   )
 }
@@ -84,22 +92,24 @@ const Search = ({query, onChange, onSubmit, children}) => {
   const [debouncedCallback] = useDebouncedCallback(onChange, 300)
 
   return (
-    <form onSubmit={onSubmit}>
-    {children}
-    {/* <select name="category" id="select-category">
-      {categories.map((value, index) => {
-        return <option key={index} value={value}>{value.charAt(0).toUpperCase() + value.substr(1)}</option>
-      })}
-    </select> */}
-    <input 
-      type="text"
-      value={query}
-      onChange={(e) => debouncedCallback(e.target.value)}
-    />
-    <button type="submit">
-      {children}
-    </button>
-  </form>
+    <Form onSubmit={(e) => e.preventDefault()}>
+      <Form.Group>
+        <InputGroup>
+          <Form.Control 
+            type="text"
+            value={query}
+            onChange={(e) => debouncedCallback(e.target.value)}
+            placeholder="Search for fish, bugs, fossils, and villagers"
+          />
+          <InputGroup.Append>
+            <Button variant="info" type="submit">
+              {children}
+            </Button>
+          </InputGroup.Append>
+        </InputGroup>
+        
+      </Form.Group>
+    </Form>
   )
 }
   
